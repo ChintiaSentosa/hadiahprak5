06da79df19d26928f145d95442448c47ad19a08d
ChintiaSentosa <66993826+ChintiaSentosa@users.noreply.github.com>
2021-11-04 12:16:39 +0800

    Add files via upload

---
+++ tipe36.java
@@ -0,0 +1,30 @@
+//Chintia Sentosa
+//51019004
+
+class tipe36 extends PatokInput {
+	private int harga;
+	private int kembalian;
+	
+	public void setHarga() {
+		
+		if (blok == 'A'){
+			harga=500000;
+		} else 
+		if (blok == 'B'){
+			harga=300000;
+		}
+		
+	}
+	
+	public int getHarga(){
+		return harga;
+	}
+	
+	public void setKembalian(int bayar) {
+		kembalian=bayar-harga;
+	}
+	
+	public int getKembalian() {
+		return kembalian;
+	}
+}
